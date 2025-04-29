b1cf799e8adf205dfeadcef44313d9bfea38e849
Nathan <natefintak@gmail.com>
2025-04-29 14:36:33 -0400

    controller classes in progress

--- src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
@@ -5,7 +5,9 @@ import com.zipcodewilmington.bakery.services.BakerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class BakerController {
 
     @Autowired
